9a21f8220a72557caaa030b375362c425e17bdc9
Ferdie De Oliveira <deferdie@gmail.com>
2023-12-02 21:19:49 +0000

    ignore exception

--- src/AcumenLogger/config/acumen.php
+++ src/AcumenLogger/config/acumen.php
@@ -6,6 +6,6 @@ return [
      * The exceptions that should be ignored and not reported.
      */
     'ignore_exceptions' => [
-        \Symfony\Component\HttpKernel\Exception\NotFoundHttpException::class,
+        Symfony\Component\HttpKernel\Exception\NotFoundHttpException::class,
     ]
 ];
